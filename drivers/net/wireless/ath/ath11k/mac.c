f45cbcf0ed93526cc7e7c82d7b291f48cec304a4
Aloka Dixit <quic_alokad@quicinc.com>
2023-05-05 16:11:27 +0300

    wifi: ath11k: driver settings for MBSSID and EMA
    Advertise the driver support for multiple BSSID (MBSSID) and
    enhanced multi-BSSID advertisements (EMA) by setting extended
    capabilities.
    
    Configure mbssid_max_interfaces and ema_max_profile_periodicity
    fields in structure wiphy which are used to advertise maximum number
    of interfaces and profile periodicity supported by the driver.
    
    Add new WMI fields to configure maximum vdev count supported for
    MBSSID and profile periodicity in case of EMA.
    Setting WMI_RSRC_CFG_FLAG2_CALC_NEXT_DTIM_COUNT_SET flag
    indicates that the firmware should track and update the DTIM counts
    for each non-transmitted profile.
    
    Tested-on: IPQ8074 hw2.0 AHB WLAN.HK.2.7.0.1-01744-QCAHKSWPL_SILICONZ-1
    
    Signed-off-by: Aloka Dixit <quic_alokad@quicinc.com>
    Co-developed-by: John Crispin <john@phrozen.org>
    Signed-off-by: John Crispin <john@phrozen.org>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://lore.kernel.org/r/20230405221648.17950-2-quic_alokad@quicinc.com

--- drivers/net/wireless/ath/ath11k/mac.c
+++ drivers/net/wireless/ath/ath11k/mac.c
@@ -8966,19 +8966,23 @@ static int ath11k_mac_setup_iface_combinations(struct ath11k *ar)
 
 static const u8 ath11k_if_types_ext_capa[] = {
 	[0] = WLAN_EXT_CAPA1_EXT_CHANNEL_SWITCHING,
+	[2] = WLAN_EXT_CAPA3_MULTI_BSSID_SUPPORT,
 	[7] = WLAN_EXT_CAPA8_OPMODE_NOTIF,
 };
 
 static const u8 ath11k_if_types_ext_capa_sta[] = {
 	[0] = WLAN_EXT_CAPA1_EXT_CHANNEL_SWITCHING,
+	[2] = WLAN_EXT_CAPA3_MULTI_BSSID_SUPPORT,
 	[7] = WLAN_EXT_CAPA8_OPMODE_NOTIF,
 	[9] = WLAN_EXT_CAPA10_TWT_REQUESTER_SUPPORT,
 };
 
 static const u8 ath11k_if_types_ext_capa_ap[] = {
 	[0] = WLAN_EXT_CAPA1_EXT_CHANNEL_SWITCHING,
+	[2] = WLAN_EXT_CAPA3_MULTI_BSSID_SUPPORT,
 	[7] = WLAN_EXT_CAPA8_OPMODE_NOTIF,
 	[9] = WLAN_EXT_CAPA10_TWT_RESPONDER_SUPPORT,
+	[10] = WLAN_EXT_CAPA11_EMA_SUPPORT,
 };
 
 static const struct wiphy_iftype_ext_capab ath11k_iftypes_ext_capa[] = {
@@ -9215,6 +9219,9 @@ static int __ath11k_mac_register(struct ath11k *ar)
 	if (ab->hw_params.ftm_responder)
 		wiphy_ext_feature_set(ar->hw->wiphy,
 				      NL80211_EXT_FEATURE_ENABLE_FTM_RESPONDER);
+
+	ar->hw->wiphy->mbssid_max_interfaces = TARGET_NUM_VDEVS(ab);
+	ar->hw->wiphy->ema_max_profile_periodicity = TARGET_EMA_MAX_PROFILE_PERIOD;
 
 	ath11k_reg_init(ar);
 
