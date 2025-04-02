a5b4edf9d1ab4eb657cd40d1953cc6dc00c01b33
Johan Hovold <johan+linaro@kernel.org>
2025-04-02 14:06:50 +0200

    fixup: firmware: arm_scmi: enable FC level-get quirk for X1E
    The quirk implementation does not support matching on the SoC (fallback)
    compatible yet so enable the fastchannel perf level get quirk based on
    vendor and protocol version.
    
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>

--- drivers/firmware/arm_scmi/quirks.c
+++ drivers/firmware/arm_scmi/quirks.c
@@ -88,7 +88,7 @@ struct scmi_quirk {
 /* Global Quirks Definitions */
 DEFINE_SCMI_QUIRK(clock_rates_triplet_out_of_spec, NULL, NULL, NULL, NULL);
 DEFINE_SCMI_QUIRK(perf_level_get_fc_force,
-		  NULL, "bad-vend", NULL, "0x20000-");
+		  NULL, "Qualcomm", NULL, "0x20000-");
 
 /*
  * Quirks Pointers Array
