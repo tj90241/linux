69505e65dc3789551ae1c54e5068be79ee69713a
Tyler J. Stachecki <stachecki.tyler@gmail.com>
2025-04-18 13:19:11 -0400

    ath12k: HACK: Skip rfkill configuration
    According to SL7's DSDT, rfkill is supposed to be disabled.
    As this information is not encoded in the devicetree,
    there's no hint to disable rfkill similarly in Linux right
    now. Hence, we just hard-code the disablement in the
    source code for now instead.
    
    Signed-off-by: Tyler J. Stachecki <stachecki.tyler@gmail.com>

--- drivers/net/wireless/ath/ath12k/core.c
+++ drivers/net/wireless/ath/ath12k/core.c
@@ -30,6 +30,9 @@ static int ath12k_core_rfkill_config(struct ath12k_base *ab)
 	if (!(ab->target_caps.sys_cap_info & WMI_SYS_CAP_INFO_RFKILL))
 		return 0;
 
+	/* HACK: always ignore rfkill config on the SL7 */
+	return 0;
+
 	for (i = 0; i < ab->num_radios; i++) {
 		ar = ab->pdevs[i].ar;
 
