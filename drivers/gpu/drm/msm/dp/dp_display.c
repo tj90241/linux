56e219c1728ad73e09f1474346c546bc4096ca25
Aleksandrs Vinarskis <alex.vinarskis@gmail.com>
2025-04-30 02:09:45 +0200

    drm/msm/dp: Account for LTTPRs capabilities
    Take into account LTTPR capabilities when selecting maximum allowed
    link rate, number of data lines.
    
    Fixes: 72d0af4accd9 ("drm/msm/dp: Add support for LTTPR handling")
    
    Reviewed-by: Abel Vesa <abel.vesa@linaro.org>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Signed-off-by: Aleksandrs Vinarskis <alex.vinarskis@gmail.com>
    Tested-by: Johan Hovold <johan+linaro@kernel.org>
    Tested-by: Rob Clark <robdclark@gmail.com>
    Reviewed-by: Abhinav Kumar <quic_abhinavk@quicinc.com>
    Signed-off-by: Tyler J. Stachecki <stachecki.tyler@gmail.com>

--- drivers/gpu/drm/msm/dp/dp_display.c
+++ drivers/gpu/drm/msm/dp/dp_display.c
@@ -360,13 +360,12 @@ static int dp_display_send_hpd_notification(struct dp_display_private *dp,
 
 static void dp_display_lttpr_init(struct dp_display_private *dp, u8 *dpcd)
 {
-	u8 lttpr_caps[DP_LTTPR_COMMON_CAP_SIZE];
 	int rc;
 
-	if (drm_dp_read_lttpr_common_caps(dp->aux, dpcd, lttpr_caps))
+	if (drm_dp_read_lttpr_common_caps(dp->aux, dpcd, dp->link->lttpr_common_caps))
 		return;
 
-	rc = drm_dp_lttpr_init(dp->aux, drm_dp_lttpr_count(lttpr_caps));
+	rc = drm_dp_lttpr_init(dp->aux, drm_dp_lttpr_count(dp->link->lttpr_common_caps));
 	if (rc)
 		DRM_ERROR("failed to set LTTPRs transparency mode, rc=%d\n", rc);
 }
--- drivers/gpu/drm/msm/dp/dp_panel.c
+++ drivers/gpu/drm/msm/dp/dp_panel.c
@@ -47,7 +47,7 @@ static void dp_panel_read_psr_cap(struct dp_panel_private *panel)
 
 static int dp_panel_read_dpcd(struct dp_panel *dp_panel)
 {
-	int rc;
+	int rc, max_lttpr_lanes, max_lttpr_rate;
 	struct dp_panel_private *panel;
 	struct dp_link_info *link_info;
 	u8 *dpcd, major, minor;
@@ -74,6 +74,16 @@ static int dp_panel_read_dpcd(struct dp_panel *dp_panel)
 	/* Limit link rate from link-frequencies of endpoint property of dtsi */
 	if (link_info->rate > dp_panel->max_dp_link_rate)
 		link_info->rate = dp_panel->max_dp_link_rate;
+
+	/* Limit data lanes from LTTPR capabilities, if any */
+	max_lttpr_lanes = drm_dp_lttpr_max_lane_count(panel->link->lttpr_common_caps);
+	if (max_lttpr_lanes && max_lttpr_lanes < link_info->num_lanes)
+		link_info->num_lanes = max_lttpr_lanes;
+
+	/* Limit link rate from LTTPR capabilities, if any */
+	max_lttpr_rate = drm_dp_lttpr_max_link_rate(panel->link->lttpr_common_caps);
+	if (max_lttpr_rate && max_lttpr_rate < link_info->rate)
+		link_info->rate = max_lttpr_rate;
 
 	drm_dbg_dp(panel->drm_dev, "version: %d.%d\n", major, minor);
 	drm_dbg_dp(panel->drm_dev, "link_rate=%d\n", link_info->rate);
