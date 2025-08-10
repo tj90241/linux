4034647d4de6521a1ac976625bf4babdc7d31a19
Akhil P Oommen <akhilpo@quicinc.com>
2025-08-10 10:49:03 -0400

    drm/msm: Add a ftrace for GPU register access
    With IFPC, there is a probability of accessing a GX domain register when
    it is collapsed, which leads to GMU fence errors. To debug this, we need
    to trace every GPU register accesses and identify the one just before a
    GMU fence error. So, add an ftrace to track all GPU register accesses.
    
    Signed-off-by: Akhil P Oommen <akhilpo@quicinc.com>
    Signed-off-by: Tyler J. Stachecki <stachecki.tyler@gmail.com>

--- drivers/gpu/drm/msm/msm_gpu_trace.h
+++ drivers/gpu/drm/msm/msm_gpu_trace.h
@@ -205,6 +205,18 @@ TRACE_EVENT(msm_gpu_preemption_irq,
 		TP_printk("preempted to %u", __entry->ring_id)
 );
 
+TRACE_EVENT(msm_gpu_regaccess,
+		TP_PROTO(u32 offset),
+		TP_ARGS(offset),
+		TP_STRUCT__entry(
+			__field(u32, offset)
+			),
+		TP_fast_assign(
+			__entry->offset = offset;
+			),
+		TP_printk("offset=0x%x", __entry->offset)
+);
+
 #endif
 
 #undef TRACE_INCLUDE_PATH
